0006b6fcebaca8165e23801cd8a3c7af97361616
nayrass <nayrass48@educastur.es>
2025-10-16 17:47:45 +0200

    cambio a resta

--- src/ejercicios/Ejemplo1While.java
+++ src/ejercicios/Ejemplo1While.java
@@ -4,9 +4,9 @@ import java.util.Scanner;
 public class Ejemplo1While {
     public static void main(String[] args) {
 
-        private int suma,contador;
+        private int resta,contador;
         public Ejemplo1While(){
-            this.suma=0;
+            this.resta=0;
             this.contador=0;
         }
         int suma = 0, num;
