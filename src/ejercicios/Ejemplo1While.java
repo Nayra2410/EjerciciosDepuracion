3ecfc46ddec1760611e1ad0e31888eecfff0545f
nayrass <nayrass48@educastur.es>
2025-10-16 17:38:51 +0200

    cambio a contador

--- src/ejercicios/Ejemplo1While.java
+++ src/ejercicios/Ejemplo1While.java
@@ -4,10 +4,10 @@ import java.util.Scanner;
 public class Ejemplo1While {
     public static void main(String[] args) {
 
-        private int suma,contador;
+        private int suma,numero;
         public Ejemplo1While(){
             this.suma=0;
-            this.contador=0;
+            this.numero=0;
         }
         int suma = 0, num;
         Scanner sc = new Scanner(System.in);
